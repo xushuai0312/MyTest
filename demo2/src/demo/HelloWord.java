3ac2b9b2a54d28991dd5923b7ecedb8ed4962da4
xsgit <987523846@qq.com>
2018-01-06 22:00:41 +0800

    11

---
+++ demo2/src/demo/HelloWord.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("helloWorld");
+
+		System.out.println("xxxxxxxxxxxx");
+
+		System.out.println("111111111111111");
+
+	}
+}
